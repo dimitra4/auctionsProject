c8052030e61390570a99420dcdbdc7dc0093dfb8
dimitra4 <dimkos1996@yahoo.gr>
2022-09-18 23:39:49 +0300

    My first commit

---
+++ src/test/java/pms/di/uoa/ecommerce/auctions/AuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pms.di.uoa.ecommerce.auctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
