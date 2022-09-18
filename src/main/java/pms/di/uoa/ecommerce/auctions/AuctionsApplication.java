332ee5dc849a7d3e0f0d83be8f47403d1ec7d71a
dimitra4 <dimkos1996@yahoo.gr>
2022-09-18 23:48:04 +0300

    My first commit

--- src/main/java/pms/di/uoa/ecommerce/auctions/AuctionsApplication.java
+++ src/main/java/pms/di/uoa/ecommerce/auctions/AuctionsApplication.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class AuctionsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(AuctionsApplication.class, args);
     }
